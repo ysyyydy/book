07711c1ac84f3fcbc5269e5d66157be8fe212421
XIE <2719864421@qq.com>
2023-04-10 01:38:07 +0800

    测试文件4

--- app/src/androidTest/java/com/example/book/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/book/ExampleInstrumentedTest.java
@@ -1,16 +1,18 @@
 package com.example.book;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
 /**
  * Instrumented test, which will execute on an Android device.
+ *====
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
