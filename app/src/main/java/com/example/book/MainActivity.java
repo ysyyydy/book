8b52223dcaeb6fbacd20d85e1e16eb9c2dff0699
pjy <2982193891@qq.com>
2023-04-14 10:07:35 +0800

    test_pjy

--- app/src/main/java/com/example/book/MainActivity.java
+++ app/src/main/java/com/example/book/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // menu should be considered as top level destinations....
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
